ea37ab600b8a78bc3e719e7a1a62737107cf475d
cssivision <cssivision@gmail.com>
2021-06-20 07:01:28 +0000

    remove deadcode

--- src/driver/accept.rs
+++ src/driver/accept.rs
@@ -1,38 +1,25 @@
 use std::io;
-use std::mem::{size_of, MaybeUninit};
 use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::os::unix::io::RawFd;
+use std::ptr;
 
 use io_uring::{opcode, types};
 
 use crate::driver::Action;
 
-pub(crate) struct Accept {
-    sockaddr: Box<MaybeUninit<libc::sockaddr_storage>>,
-}
-
-impl Accept {
-    pub fn get_socketaddr(&self) -> io::Result<SocketAddr> {
-        let addr = unsafe { to_socket_addr(self.sockaddr.as_ptr()) };
-        let _sockaddr = unsafe { (*self.sockaddr).assume_init() };
-        addr
-    }
-}
+pub(crate) struct Accept;
 
 impl Action<Accept> {
     pub(crate) fn accept(fd: RawFd) -> io::Result<Action<Accept>> {
-        let mut sockaddr = Box::new(MaybeUninit::uninit());
-        let mut length = size_of::<libc::sockaddr_storage>() as libc::socklen_t;
-        let entry =
-            opcode::Accept::new(types::Fd(fd), sockaddr.as_mut_ptr() as *mut _, &mut length)
-                .flags(libc::SOCK_CLOEXEC)
-                .build();
+        let entry = opcode::Accept::new(types::Fd(fd), ptr::null_mut(), ptr::null_mut())
+            .flags(libc::SOCK_CLOEXEC)
+            .build();
 
-        Action::submit(Accept { sockaddr }, entry)
+        Action::submit(Accept, entry)
     }
 }
 
-unsafe fn to_socket_addr(storage: *const libc::sockaddr_storage) -> io::Result<SocketAddr> {
+unsafe fn _to_socket_addr(storage: *const libc::sockaddr_storage) -> io::Result<SocketAddr> {
     match (*storage).ss_family as libc::c_int {
         libc::AF_INET => {
             // Safety: if the ss_family field is AF_INET then storage must be a sockaddr_in.
