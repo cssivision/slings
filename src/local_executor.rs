9c9a93cef2e3c93f0f3560bdb37dc2588179c787
cssivision <cssivision@gmail.com>
2021-07-01 14:55:39 +0800

    bump 0.2.4

--- src/local_executor.rs
+++ src/local_executor.rs
@@ -1,62 +1,37 @@
 use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::future::Future;
-use std::rc::Rc;
 
 use async_task::{Runnable, Task};
-use scoped_tls::scoped_thread_local;
 
 const MAX_TASKS_PER_TICK: usize = 64;
 
-pub struct LocalExecutor {
-    queue: Rc<RefCell<VecDeque<Runnable>>>,
+thread_local! {
+    static GLOBAL_QUEUE: RefCell<VecDeque<Runnable>> = RefCell::new(VecDeque::with_capacity(64));
 }
 
-scoped_thread_local!(static EXECUTOR: LocalExecutor);
+pub fn tick() -> bool {
+    for _ in 0..MAX_TASKS_PER_TICK {
+        match next_task() {
+            Some(task) => {
+                task.run();
+            }
+            None => return false,
+        }
+    }
+    true
+}
+
+fn next_task() -> Option<Runnable> {
+    GLOBAL_QUEUE.with(|queue| queue.borrow_mut().pop_front())
+}
 
 pub fn spawn_local<T: 'static>(future: impl Future<Output = T> + 'static) -> Task<T> {
-    if !EXECUTOR.is_set() {
-        panic!("`spawn_local` called from outside of a `LocalExecutor`");
-    }
+    let schedule = move |runnable| {
+        let _ = GLOBAL_QUEUE.with(|queue| queue.borrow_mut().push_back(runnable));
+    };
 
-    EXECUTOR.with(|local_executor| local_executor.spawn_local(future))
-}
-
-impl LocalExecutor {
-    pub fn new() -> LocalExecutor {
-        LocalExecutor {
-            queue: Rc::new(RefCell::new(VecDeque::with_capacity(64))),
-        }
-    }
-
-    pub fn tick(&self) -> bool {
-        for _ in 0..MAX_TASKS_PER_TICK {
-            match self.next_task() {
-                Some(task) => {
-                    task.run();
-                }
-                None => return false,
-            }
-        }
-        true
-    }
-
-    fn next_task(&self) -> Option<Runnable> {
-        self.queue.borrow_mut().pop_front()
-    }
-
-    pub fn spawn_local<T: 'static>(&self, future: impl Future<Output = T> + 'static) -> Task<T> {
-        let queue = self.queue.clone();
-        let schedule = move |runnable| {
-            let _ = queue.borrow_mut().push_back(runnable);
-        };
-
-        let (runnable, task) = unsafe { async_task::spawn_unchecked(future, schedule) };
-        runnable.schedule();
-        task
-    }
-
-    pub(crate) fn with<T>(&self, f: impl FnOnce() -> T) -> T {
-        EXECUTOR.set(self, f)
-    }
+    let (runnable, task) = unsafe { async_task::spawn_unchecked(future, schedule) };
+    runnable.schedule();
+    task
 }
--- src/runtime.rs
+++ src/runtime.rs
@@ -3,27 +3,16 @@ use std::io;
 use std::task::{Context, Poll};
 
 use crate::driver::Driver;
-use crate::local_executor::LocalExecutor;
+use crate::local_executor;
 use crate::waker_fn::waker_fn;
 
-/// Runs a closure when dropped.
-struct CallOnDrop<F: Fn()>(F);
-
-impl<F: Fn()> Drop for CallOnDrop<F> {
-    fn drop(&mut self) {
-        (self.0)();
-    }
-}
-
 pub struct Runtime {
-    local_executor: LocalExecutor,
     driver: Driver,
 }
 
 impl Runtime {
     pub fn new() -> io::Result<Runtime> {
         Ok(Runtime {
-            local_executor: LocalExecutor::new(),
             driver: Driver::new()?,
         })
     }
@@ -33,22 +22,17 @@ impl Runtime {
         F: Future,
     {
         pin_mut!(future);
-
         let waker = waker_fn(|| {});
         let cx = &mut Context::from_waker(&waker);
 
-        self.driver.with(|| {
-            self.local_executor.with(|| loop {
-                if let Poll::Ready(output) = future.as_mut().poll(cx) {
-                    return output;
-                }
-
-                if self.local_executor.tick() {
-                    continue;
-                }
-
-                self.driver.wait().expect("driver wait error");
-            })
+        self.driver.with(|| loop {
+            if let Poll::Ready(output) = future.as_mut().poll(cx) {
+                return output;
+            }
+            if local_executor::tick() {
+                continue;
+            }
+            self.driver.wait().expect("driver wait error");
         })
     }
 }
