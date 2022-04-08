c727cfc52855837f730eddd235d0daece1fecd9b
cssivision <cssivision@gmail.com>
2022-04-08 12:51:18 +0000

    rename addr.rs => socketaddr.rs

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -1,9 +1,9 @@
-mod addr;
 mod packet;
 mod socket;
+mod socketaddr;
 mod stream;
 
-pub(crate) use addr::SocketAddr;
 pub(crate) use packet::Packet;
 pub(crate) use socket::Socket;
+pub(crate) use socketaddr::SocketAddr;
 pub(crate) use stream::Stream;
