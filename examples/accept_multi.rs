8f6e3f1991df77f59eff36c136a3dba51a657309
cssivision <cssivision@gmail.com>
2023-04-24 19:44:51 +0800

    add example

---
+++ examples/accept_multi.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::net::SocketAddr;
+use std::time::Duration;
+
+use futures_util::stream::StreamExt;
+use futures_util::AsyncWriteExt;
+use slings::net::TcpListener;
+use slings::time::delay_for;
+
+fn main() -> io::Result<()> {
+    slings::block_on(async {
+        let addr: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+        let listener = TcpListener::bind(addr)?;
+        println!("server start listen on {:?}", listener.local_addr());
+        let mut incoming = listener.accept_multi();
+        while let Some(stream) = incoming.next().await {
+            let (mut stream, addr) = stream?;
+            println!("accept stream from addr: {:?}", addr);
+            slings::spawn_local(async move {
+                loop {
+                    let buf = b"helloworld";
+                    match stream.write_all(&buf[..]).await {
+                        Ok(_) => {
+                            println!("write 10 bytes");
+                        }
+                        Err(e) => {
+                            println!("write fail {}", e);
+                            break;
+                        }
+                    }
+                    delay_for(Duration::from_secs(1)).await;
+                }
+            })
+            .detach();
+        }
+        Ok(())
+    })
+}
